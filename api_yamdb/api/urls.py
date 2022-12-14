4ae54d5fef4b878f074d396d7961278133eb3b8c
Клавдия Дунаева <klavam@yandex.ru>
2022-12-14 14:01:47 +0700

    создание моделей,урлов.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,19 +1,17 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import TitleViewSet, CategoriesViewSet, GenresViewSet
+from .views import TitlesViewSet, CategoriesViewSet, GenresViewSet
 
 router_v1 = routers.DefaultRouter()
-router_v1.register(r'titles', TitleViewSet, basename='titles')
-router_v1.register(r'categories', CategoriesViewSet, basename='categoreis')
-router_v1.register(r'categories/', GroupViewSet, basename='groups')
-router_v1.register(r'posts/(?P<post_id>\d+)/comments',
-                   CommentViewSet,
-                   basename='comments')
-router_v1.register(r'follow', FollowViewSet,
-                   basename='follow')
+router_v1.register(r'titles', TitlesViewSet, basename='titles')
+router_v1.register(r'categories', CategoriesViewSet, basename='categories')
+# router_v1.register(r'categories/{slug}', GroupViewSet, basename='groups')
+router_v1.register(r'genres', GenresViewSet, basename='genres')
+# router_v1.register(r'genres/{slug}', GenresViewSet, basename='genres')
+
 
 urlpatterns = [
-    path('v1/', include(router_v1.urls)),
-    path('v1/', include('djoser.urls.jwt')),
+    path('', include(router_v1.urls)),
+    # path('v1/', include('djoser.urls.jwt')),
 ]
