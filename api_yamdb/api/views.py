f01c7166d0c7a38b30d18c5194d2f321b151b771
Nikolay <delonartt@gmail.com>
2022-12-14 19:20:43 +0500

    second commit

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -6,7 +6,8 @@ from rest_framework.permissions import IsAuthenticated
 
 # from api.permissions import IsOwnerOrReadOnly
 
-from ..reviews.models import Categories, Titles, Genres
+from ..reviews.models import Categories, Titles, Genres, Review, Comment
+from api.serializers import CommentSerializer
 
 
 class CategoriesViewSet(viewsets.ReadOnlyModelViewSet):
@@ -58,3 +59,16 @@ class GenresViewSet(viewsets.ModelViewSet):
 #     def perform_create(self, serializer):
 #         serializer.save(user=self.request.user)
 
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    # permission_classes = (OwnerOrReadOnly,)
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        new_queryset = Comment.objects.filter(post=title_id)
+        return new_queryset
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Titles, id=title_id)
+        serializer.save(author=self.request.user, title=title)
