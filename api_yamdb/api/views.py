f956bf2e3557a91fae8ec4748b010b33adf5348e
Nikolay <delonartt@gmail.com>
2022-12-17 09:46:55 +0500

    6 commit

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -48,6 +48,16 @@ class GenresViewSet(CreateListDestroyViewSet):
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
 
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        new_queryset = Review.objects.filter(post=title_id)
+        return new_queryset
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Titles, id=title_id)
+        serializer.save(author=self.request.user, title=title)
+
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
