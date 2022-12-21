8bcb1b6be19bb65cb6aa0f018a61b942836b40c4
Клавдия Дунаева <klavam@yandex.ru>
2022-12-21 17:02:44 +0700

    исправила фильтры

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -4,7 +4,8 @@ from reviews.models import Title
 
 
 class TitleFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
+    name = django_filters.CharFilter(field_name='name',
+                                     lookup_expr='icontains')
     year = django_filters.CharFilter(field_name='year')
     category = django_filters.CharFilter(field_name='category__slug')
     genre = django_filters.CharFilter(field_name='genre__slug')
